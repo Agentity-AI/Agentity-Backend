1917c62539f9e295ef106162de3674abb775dd08
Samuel Ilelakinwa <ilelakinwa.samuel@gmail.com>
2026-02-20 12:25:32 +0100

    production ready sequelize DB

--- src/config/database.js
+++ src/config/database.js
@@ -3,24 +3,31 @@ require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+let sequelize;
+
+if (isProduction && process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
-    dialectOptions: isProduction
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : {},
-  }
-);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      dialect: "postgres",
+      logging: false,
+    },
+  );
+}
 
 module.exports = sequelize;
